a7139c1d3eff00dd54264876a1c543447503a1a6
Tal Aviram <me@talaviram.com>
2022-10-22 15:56:07 -0400

    Fix US state scrapping ignoring some states tax.

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -2,6 +2,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+state_sales_tax = requests.get("https://www.tax-rates.org/taxtables/sales-tax-by-state")
 local_sales_tax = requests.get("https://www.tax-rates.org/taxtables/local-sales-taxes")
 node_sales_tax = requests.get(
     "https://raw.githubusercontent.com/valeriansaliou/node-sales-tax/master/res/sales_tax_rates.json"
@@ -10,58 +11,58 @@ node_sales_tax = requests.get(
 grabbed_tax = {}
 
 states_to_code = {
-    "Alabama": "AL",
-    "Alaska": "AK",
-    "Arizona": "AZ",
-    "Arkansas": "AR",
-    "California": "CA",
-    "Colorado": "CO",
-    "Connecticut": "CT",
-    "District Of Columbia": "DC",
-    "Delaware": "DE",
-    "Florida": "FL",
-    "Georgia": "GA",
-    "Hawaii": "HI",
-    "Idaho": "ID",
-    "Illinois": "IL",
-    "Indiana": "IN",
-    "Iowa": "IA",
-    "Kansas": "KS",
-    "Kentucky": "KY",
-    "Louisiana": "LA",
-    "Maine": "ME",
-    "Maryland": "MD",
-    "Massachusetts": "MA",
-    "Michigan": "MI",
-    "Minnesota": "MN",
-    "Mississippi": "MS",
-    "Missouri": "MO",
-    "Montana": "MT",
-    "Nebraska": "NE",
-    "Nevada": "NV",
-    "New Hampshire": "NH",
-    "New Jersey": "NJ",
-    "New Mexico": "NM",
-    "New York": "NY",
-    "North Carolina": "NC",
-    "North Dakota": "ND",
-    "Ohio": "OH",
-    "Oklahoma": "OK",
-    "Oregon": "OR",
-    "Pennsylvania": "PA",
-    "Puerto Rico": "PR",
-    "Rhode Island": "RI",
-    "South Carolina": "SC",
-    "South Dakota": "SD",
-    "Tennessee": "TN",
-    "Texas": "TX",
-    "Utah": "UT",
-    "Vermont": "VT",
-    "Virginia": "VA",
-    "Washington": "WA",
-    "West Virginia": "WV",
-    "Wisconsin": "WI",
-    "Wyoming": "WY",
+    "ALABAMA": "AL",
+    "ALASKA": "AK",
+    "ARIZONA": "AZ",
+    "ARKANSAS": "AR",
+    "CALIFORNIA": "CA",
+    "COLORADO": "CO",
+    "CONNECTICUT": "CT",
+    "DISTRICT OF COLUMBIA": "DC",
+    "DELAWARE": "DE",
+    "FLORIDA": "FL",
+    "GEORGIA": "GA",
+    "HAWAII": "HI",
+    "IDAHO": "ID",
+    "ILLINOIS": "IL",
+    "INDIANA": "IN",
+    "IOWA": "IA",
+    "KANSAS": "KS",
+    "KENTUCKY": "KY",
+    "LOUISIANA": "LA",
+    "MAINE": "ME",
+    "MARYLAND": "MD",
+    "MASSACHUSETTS": "MA",
+    "MICHIGAN": "MI",
+    "MINNESOTA": "MN",
+    "MISSISSIPPI": "MS",
+    "MISSOURI": "MO",
+    "MONTANA": "MT",
+    "NEBRASKA": "NE",
+    "NEVADA": "NV",
+    "NEW HAMPSHIRE": "NH",
+    "NEW JERSEY": "NJ",
+    "NEW MEXICO": "NM",
+    "NEW YORK": "NY",
+    "NORTH CAROLINA": "NC",
+    "NORTH DAKOTA": "ND",
+    "OHIO": "OH",
+    "OKLAHOMA": "OK",
+    "OREGON": "OR",
+    "PENNSYLVANIA": "PA",
+    "PUERTO RICO": "PR",
+    "RHODE ISLAND": "RI",
+    "SOUTH CAROLINA": "SC",
+    "SOUTH DAKOTA": "SD",
+    "TENNESSEE": "TN",
+    "TEXAS": "TX",
+    "UTAH": "UT",
+    "VERMONT": "VT",
+    "VIRGINIA": "VA",
+    "WASHINGTON": "WA",
+    "WEST VIRGINIA": "WV",
+    "WISCONSIN": "WI",
+    "WYOMING": "WY",
 }
 
 
@@ -95,21 +96,34 @@ def grab_state_web(state_url, state_tax):
     return counties
 
 
+def grab_us_states_tax():
+    states_tax = {}
+    for row in grab_table("taxBox", state_sales_tax.content):
+        row_content = row.find_all("td")
+        for row in row_content:
+            state = states_to_code[row.find("a").text.upper()]
+            rate = row.find("div").text
+            states_tax[state] = clean_percent(rate)
+    return states_tax
+
+
 def grab_us_tax_to_dict():
     grabbed_tax = {}
+    states_tax = grab_us_states_tax()
     for row in grab_table("taxBox", local_sales_tax.content):
         row_content = row.find_all("td")
         for row_el in row_content:
             state = row_el.find("a")
             tax = row_el.find("div")
             if state and tax:
+                state_code = states_to_code[state.text.upper()]
                 tax_value = clean_percent(tax.text)
                 counties = {
-                    "rate": tax_value,
+                    "rate": max(tax_value, states_tax[state_code]),
                     "type": "vat",
                     "counties": grab_state_web(state["href"], tax_value),
                 }
-                grabbed_tax.update({states_to_code[state.text]: counties})
+                grabbed_tax.update({state_code: counties})
     return grabbed_tax
 
 
